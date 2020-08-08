4547868fefaf8fb25fd9033428ec722c2f4d54fa
paulosilva777 <engpaulofrota@gmail.com>
2020-08-07 22:56:25 -0300

    web app finshed

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,55 @@
 import React from 'react';
 import './styles.css';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className='teacher-item'>
       <header>
-        <img
-          src='https://avatars2.githubusercontent.com/u/52797828?s=460&v=4'
-          alt='profile'
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Paulo Silva</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, enim
-        neque dignissimos laudantium asperiores qui mollitia nulla, ipsum,
-        debitis eaque recusandae a eligendi consequuntur expedita officiis
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type='button'>
+        <a
+          target='_blank'
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt='whatsapp' />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
